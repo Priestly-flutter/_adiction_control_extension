66ba53a0fcf1beaecfabc539207936d80d1495ad
JatoPriestlyNjilah <priestlynjilah@gmail.com>
2024-01-21 12:13:22 +0100

    added icon and testes the main.js file and its function

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert ("X-Control is up and running")
+console.log("X-Control is up and running your search will be monitored");
+
+var images =  document.getElementsByTagName('img')
+
+for(elt of images){
+    //dummy script
+    elt.src = '${browser.runtime.getURL("19.jpg")}'
+    elt.alt = 'an alt text'
+}
